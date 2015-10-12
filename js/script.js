c67bb4326920a17d1e90e3d8e68c49adbd7c5533
johnrdixon <johnryandixon@gmail.com>
2015-10-12 01:11:23 -0400

    js fixes

--- js/script.js
+++ js/script.js
@@ -8,15 +8,105 @@ $(document).ready(function() {
         $('.menu-link').bigSlide();
     });
 
+// $(document).ready(function() {
+//         $('#third').collapsible();
+//     });
+
+
+// document.getElementById('letter').onchange = function() {
+// document.getElementById('behalf').style.display = this.checked ? 'block' : 'none';
+// };
+
+// $(function() {
+// $('#third').collapsible('accordion');
+// });
+
 $(document).ready(function() {
-        $('#third').collapsible();
+
+	var $hide = $( "#hide" ),
+		$dropdown = $( "#dropdown_menu" );
+
+    $('.menu-link').bigSlide();
+
+    $hide.on( "click", function( e ){
+        
+        $dropdown.slideToggle( 300 );
     });
 
+    $dropdown.hide();
+});
 
-document.getElementById('letter').onchange = function() {
-document.getElementById('behalf').style.display = this.checked ? 'block' : 'none';
-};
+$(document).ready(function() {
 
-$(function() {
-$('#third').collapsible('accordion');
+	var $hide = $( "#hide2" ),
+		$dropdown = $( "#dropdown_menu2" );
+
+    $('.menu-link').bigSlide();
+
+    $hide.on( "click", function( e ){
+        
+        $dropdown.slideToggle( 300 );
+    });
+
+    $dropdown.hide();
+});
+
+$(document).ready(function() {
+
+	var $hide = $( "#hide3" ),
+		$dropdown = $( "#dropdown_menu3" );
+
+    $('.menu-link').bigSlide();
+
+    $hide.on( "click", function( e ){
+        
+        $dropdown.slideToggle( 300 );
+    });
+
+    $dropdown.hide();
+});
+
+$(document).ready(function() {
+
+	var $hide = $( "#hide4" ),
+		$dropdown = $( "#dropdown_menu4" );
+
+    $('.menu-link').bigSlide();
+
+    $hide.on( "click", function( e ){
+        
+        $dropdown.slideToggle( 300 );
+    });
+
+    $dropdown.hide();
+});
+
+$(document).ready(function() {
+
+	var $hide = $( "#hide5" ),
+		$dropdown = $( "#dropdown_menu5" );
+
+    $('.menu-link').bigSlide();
+
+    $hide.on( "click", function( e ){
+        
+        $dropdown.slideToggle( 300 );
+    });
+
+    $dropdown.hide();
+});
+
+$(document).ready(function() {
+
+	var $hide = $( "#hide6" ),
+		$dropdown = $( "#dropdown_menu6" );
+
+    $('.menu-link').bigSlide();
+
+    $hide.on( "click", function( e ){
+        
+        $dropdown.slideToggle( 300 );
+    });
+
+    $dropdown.hide();
 });
